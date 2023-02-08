ea131545aa4fcc0045036a87db815d285aaa2516
Minion3468 <akcjareakcja00@gmail.com>
2023-02-08 15:24:49 +0100

    Bloodborne to zajebista gra

--- pub/sandbox/index.php
+++ pub/sandbox/index.php
@@ -19,56 +19,79 @@
     if(isset($_POST['submit']))
     {
          //zdefiniuj folder do którego trafią pliki (ścieżka względem pliku index.php)
-         $targetDir = "img/";
+        $targetDir = "img/";
 
 
-         //pobierz pierwotną nazwę pliku z tablicy $_FILES
-         $sourceFileName = $_FILES['uploadedFile']['name'];
-
-
-         //wyciągnij pierwotne rozszerzenie pliku
-        $sourceFileExtension = pathinfo($sourceFileName, PATHINFO_EXTENSION);
         
-        
+        //pobierz pierwotną nazwę pliku z tablicy $_FILES
+        $sourceFileName = $_FILES['uploadedFile']['name'];
+
+
+
+        //pobierz tymczasową ścieżkę do pliku na serwerze
+        $tempURL = $_FILES['uploadedFile']['tmp_name'];
+
+
+
+        //sprawdź czy mamy do czynienia z obrazem
+        $imgInfo = getimagesize($tempURL);
+        if(!is_array($imgInfo)) {
+            die("Błąd: Przekazany plik nie jest obrazem!");
+        }
+
+
+
+        //wyciągnij pierwotne rozszerzenie pliku
+        //$sourceFileExtension = pathinfo($sourceFileName, PATHINFO_EXTENSION);
         //zmień litery rozszerzenia na małe
-        $sourceFileExtension = strtolower($sourceFileExtension);
+        //$sourceFileExtension = strtolower($sourceFileExtension);
+        /// niepotrzebne - generujemy webp
 
-        
+
+
         //wygeneruj hash - nową nazwę pliku
         $newFileName = hash("sha256", $sourceFileName) . hrtime(true)
-                            . "." . $sourceFileExtension;
-        
-                            
+                            . ".webp";
+
+
+
+        //zaczytujemy cały obraz z folderu tymczasowego do stringa
+        $imageString = file_get_contents($tempURL);
+
+
+
+        //generujemy obraz jako obiekt klasy GDImage
+        //@ przed nazwa funkcji powoduje zignorowanie ostrzeżeń
+        $gdImage = @imagecreatefromstring($imageString);
+
+
+
         //wygeneruj pełny docelowy URL
         $targetURL = $targetDir . $newFileName;
- 
 
-         //pobierz tymczasową ścieżkę do pliku na serwerze
-         $tempURL = $_FILES['uploadedFile']['tmp_name'];
 
-         
-         //sprawdź czy mamy do czynienia z obrazem
-         $imgInfo = getimagesize($tempURL);
-         if(!is_array($imgInfo)) {
-             die("Błąd: Przekazany plik to nie obrazem!");
-         }
- 
 
-         //zbuduj docelowy URL pliku na serwerze
-         //$targetURL = $targetDir . $sourceFileName;
-         //wycofane na rzecz hasha
- 
+        //zbuduj docelowy URL pliku na serwerze
+        //$targetURL = $targetDir . $sourceFileName;
+        //wycofane na rzecz hasha
 
-         //sprawdź czy plik przypadkiem już nie istnieje
-         if(file_exists($targetURL)) {
-             die("Błąd: Podany plik już istnieje!");
-         }
- 
 
-         //przesuń plik do docelowej lokalizacji
-         move_uploaded_file($tempURL, $targetURL);
-         
-         echo "Plik został poprawnie wrzucony na serwer";
+
+        //sprawdź czy plik przypadkiem już nie istnieje
+        if(file_exists($targetURL)) {
+            die("Błąd: Podany plik już istnieje!");
+        }
+
+
+
+        //przesuń plik do docelowej lokalizacji
+        //move_uploaded_file($tempURL, $targetURL);
+        //nieaktualne - generujemy webp
+        imagewebp($gdImage, $targetURL);
+
+
+
+        echo "Plik został poprawnie wrzucony na serwer";
     }
 
 
