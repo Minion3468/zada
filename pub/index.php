cd798b8cf4fbfa2eb4b75040bcb9f7ebe3434875
Minion3468 <akcjareakcja00@gmail.com>
2023-03-15 15:22:06 +0100

    safgf

--- pub/index.php
+++ pub/index.php
@@ -4,9 +4,7 @@ require_once('./../src/config.php');
 use Steampixel\Route;
 
 Route::add('/', function() {
-    //strona wyświetlająca obrazki
     global $twig;
-    //pobierz 10 najnowszych postów
     $postArray = Post::getPage();
     $twigData = array("postArray" => $postArray,
                         "pageTitle" => "Strona główna");
@@ -14,22 +12,18 @@ Route::add('/', function() {
 });
 
 Route::add('/upload', function() {
-    //strona z formularzem do wgrywania obrazków
     global $twig;
     $twigData = array("pageTitle" => "Wgraj mema");
     $twig->display("upload.html.twig", $twigData);
 });
 
 Route::add('/upload', function() {
-    //wywoła się tylko po otrzymaniu danych metodą post na ten url
-    // (po wypełnieniu formularza)
     global $twig;
     if(isset($_POST['submit']))  {
         Post::upload($_FILES['uploadedFile']['tmp_name']);
     }
-    //TODO: zmienić na ścieżkę względną
-    header("Location: http://localhost/cms/pub");
+    header("Location: http://localhost/zada/pub");
 }, 'post');
 
-Route::run('/cms/pub');
+Route::run('/zada/pub');
 ?>
